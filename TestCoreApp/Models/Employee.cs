a2da8a847a7909faf2d2ca510a6f222d53a64d00
Y********* <admin@test.domains.com>
2025-03-17 20:52:12 +0200

    Add project files.

---
+++ TestCoreApp/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestCoreApp.Models
+{
+    public class Employee
+    {
+        [Key]
+       public int Id { get; set; }
+        [Required]
+        public string? EmployeeName { get; set; }
+        [Required]
+        public string? EmployeePhone { get; set; }
+        public string? EmployeeEmail { get; set; }
+
+        public int? EmployeeAgee { get; set; }
+        public decimal? EmployeeSalary { get; set; }
+
+
+    }
+}
